a5211744f1739675fe46237c4a06d1b6c9c7f185
jan fockaert <jan.fockaert@merapar.com>
2016-11-30 17:49:30 +0100

    On tagging the TRAVIS_BRANCH variable has the wrong value (https://github.com/travis-ci/travis-ci/issues/4745).

--- travis-build.sh
+++ travis-build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ev
 
-echo "Start checking build profile for branch \"${TRAVIS_BRANCH}\" and tag \"${TRAVIS_TAG}\""
+echo "Start checking build profile for tag \"${TRAVIS_TAG}\""
 
-if [ "x${TRAVIS_TAG}" != "x" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+if [ "x${TRAVIS_TAG}" != "x" ]; then
     echo "Release build from tag"
     mvn clean install -Dtravis=release -B -V
 else
